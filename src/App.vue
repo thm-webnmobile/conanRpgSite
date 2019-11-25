8f0aeb16e2d69e9a3e6888ae008b4e379fb96e83
FelixWiedermann <felix.wiedermann@iem.thm.de>
2019-11-25 15:07:33 +0100

    Updating basic Structure

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.jpg">
+    <b-navbar variant="dark" sticky="true" >
+      <b-navbar-nav>
+        <b-nav-item>
+
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
