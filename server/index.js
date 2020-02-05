7f1a4cf22ea7942e93c63b4899055c41b24c8fd2
jlng62 <Jochen.Lauinger@iem.thm.de>
2020-02-05 14:39:32 +0100

    Discord Oauth2

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require('path');
+const dotenv = require('dotenv').config({ path:__dirname+'/.env' });
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '..','client', 'src', 'main.js'));
+    res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
@@ -29,3 +32,34 @@ app.use((err, req, res, next) => {
         });
     }
 });
+
+
+//Discordbot
+var roles = require('./discordbot/commands/roles.js');
+
+client.login(process.env.TOKEN);
+
+client.on('ready', ()=> {
+  console.log('The bot is online!');
+});
+
+
+
+client.on("message", async message => {
+  const prefix = "_";
+
+  if(message.content.toLowerCase().startsWith(prefix + "addrole")){
+    var args = message.content.toLowerCase().split(" ");
+    console.log(args);
+    if(args[1] === 'pnp')
+    {
+      roles.addUserRole('pnp', message);
+      message.channel.send('Role successfully addded!');
+    }
+    else if(args[1] === 'exiles'){
+      roles.addUserRole('exiles', message);
+      message.channel.send('Role successfully addded!');
+    }
+  } 
+});
+
